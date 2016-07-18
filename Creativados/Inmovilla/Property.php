cd5573778e26195693377e435e96345e0f8a5892
Josevi <josevi@creativados.com>
2016-07-18 11:26:46 +0200

    merge result

--- Creativados/Inmovilla/Property.php
+++ Creativados/Inmovilla/Property.php
@@ -11,6 +11,39 @@ namespace Creativados\Inmovilla;
 
 class Property extends PropertyCall
 {
+    const ENVIRONMENT = [
+        0 => "Árboles",
+        1 => "Hospitales",
+        2 => "Tren",
+        3 => "Metro",
+        4 => "Golf",
+        5 => "Montaña",
+        6 => "Rural",
+        7 => "Costa",
+        8 => "Vallado",
+        9 => "Autobuses",
+        10 => "Centros Comerciales",
+        11 => "Tranvía",
+        12 => "Zonas Infantiles",
+        13 => "Colegios",
+        14 => "Céntrico",
+        15 => "Centro de Salud",
+        16 => "Zona de Paso"
+    ];
+    const BEAUTIFIERS = ["operation" => "keyacci"];
+    const BEAUTIFIERS_VALUES = [
+        'operation' => [
+            1 => "Vender",
+            2 => "Alquilar",
+            3 => "Traspasar",
+            4 => "Vender o Alquilar",
+            5 => "Vender o Traspasar",
+            6 => "Traspasar o Alquilar",
+            7 => "Traspasar, Vender o Alquilar",
+            8 => "Fuera de Mercado",
+            9 => "Temporada Alquiler",
+        ]
+    ];
     public $x_entorno = 0;
 
     /**
@@ -37,13 +70,24 @@ class Property extends PropertyCall
         }
     }
 
-    public function enviroment()
+    public function environment()
     {
-        $enviroment = [];
+        $environment = [];
         for ($i = 0; $i < 17; $i++) {
-            $enviroment[$i] = (($this->x_entorno & pow(2, $i)) == pow(2, $i));
+            $environment[$i] = (($this->x_entorno & pow(2, $i)) == pow(2, $i));
         }
-        return $enviroment;
+        return $environment;
+    }
+
+    public function environmentText()
+    {
+        $temp = [];
+        foreach ($this->environment() as $key => $value) {
+            if ($value) {
+                $temp[$key] = self::ENVIRONMENT[$key];
+            }
+        }
+        return $temp;
     }
 
     public function find($id)
@@ -90,5 +134,18 @@ class Property extends PropertyCall
         foreach ($values as $key => $value) {
             $this->{$key} = $value;
         }
+    }
+
+
+    public function __call($method, $params)
+    {
+        if (in_array($method, array_keys(self::BEAUTIFIERS))) {
+            return $this->beautifier($method);
+        }
+    }
+
+    private function beautifier($operation)
+    {
+        return self::BEAUTIFIERS_VALUES[$operation][$this->{self::BEAUTIFIERS[$operation]}];
     }
 }
